410c88444338881afb46a3d6f7856312c4b7269e
HamInGyu <hfire1207@gmail.com>
2021-07-05 13:32:31 +0900

    [추가]  input값에 대한 유효성 검사까지 해보기(JS로)

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+//login-form 아이디를 가진 요소부터 일단 불러보자
 const loginForm = document.querySelector("#login-form");
 
 //받아온 loginForm html 내부에서 또 querySelector 통해서 찾음!
 const nameInput = loginForm.querySelector("input");
 const loginBtn = loginForm.querySelector("button");
 
+//로그인 버튼에 대해, 유저이름 유효성 검사까지 해주자.
 function onLoginBtnClick() {
-    console.log("Hello" + nameInput.value)
+  const username = nameInput.value;
+	if(username === ""){
+		alert("Please write your name");
+	} else if (username.length > 15){
+		alert("Your name is too long.");
+	}
 }
 
+//로그인 버튼에 대한 이벤트 등록
 loginBtn.addEventListener("click", onLoginBtnClick);
