8099f74625f9c9eefeade9acf4479fea55e9713d
HamInGyu <hfire1207@gmail.com>
2021-07-05 15:03:30 +0900

    [수정] input값에 대한 유효성 검사, 제출 핸들링 (form태그 활용해서)

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-//login-form 아이디를 가진 요소부터 일단 불러보자
 const loginForm = document.querySelector("#login-form");
-
-//받아온 loginForm html 내부에서 또 querySelector 통해서 찾음!
 const nameInput = loginForm.querySelector("input");
-const loginBtn = loginForm.querySelector("button");
 
-//로그인 버튼에 대해, 유저이름 유효성 검사까지 해주자.
-function onLoginBtnClick() {
-  const username = nameInput.value;
-	if(username === ""){
-		alert("Please write your name");
-	} else if (username.length > 15){
-		alert("Your name is too long.");
-	}
+//이벤트에 대한 정보도 받아서 활용할 수 있도록, event를 인자로 받아 사용하자.
+//JS가 실행시켜주니, event를 받을 자리만 만들어두자!
+function onLoginSubmit(event) {
+	event.preventDefault(); //event에 대한 브라우저의 기본 동작을 막아준다! (submit -> 새로고침)
+  
+	const username = nameInput.value;
+	console.log(username);
 }
 
-//로그인 버튼에 대한 이벤트 등록
-loginBtn.addEventListener("click", onLoginBtnClick);
+//submit은 텍스트에 대해 엔터를 누르거나, form 내의 버튼을 눌렀을 때 이루어진다!
+loginForm.addEventListener("submit", onLoginSubmit);
+//eventListener는 두번째 인자인 함수를 호출할 때,
+//빈값으로 [ex. onLoginSubmit() ] 부르는 것이 아니다!
+//인자로 발생한 이벤트에 대한 정보를 담아 보낸다! [ex. onLoginSubmit(event) ]
