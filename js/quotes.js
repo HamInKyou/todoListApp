2b35331f5f78250fe46afcef3b7bc8fcedbc4322
HamInGyu <hfire1207@gmail.com>
2021-07-06 11:53:13 +0900

    [추가] 랜덤으로 명언 띄워주는 기능 구현

--- js/quotes.js
+++ js/quotes.js
@@ -43,3 +43,9 @@ const quotes = [
     author: "W. Clement Stone",
   },
 ];
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
