c74cc49a6ed475e44f34a6d1cddebbe7c300ded8
HamInGyu <hfire1207@gmail.com>
2021-07-07 16:38:57 +0900

    [추가] 임시저장한거 화면에 뿌리기

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,20 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span); //생성한 li태그 안에 span태그를 집어넣는다!
+  span.innerText = newTodo; //생성된 span태그 안에 인자로 받아온 toDo집어넣기
+  toDoList.appendChild(li); //그렇게 만들어놓은 toDo 하나를, toDoList 안에 집어넣기!
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault(); //submit 기본 액션->새로고침 방지
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+
+  paintToDo(newTodo); //저장한 toDo 화면에 그려주기
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
