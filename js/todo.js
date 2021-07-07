e9e1b9f9aa2ba61a7fedc0a7ca4ecb90a7552401
HamInGyu <hfire1207@gmail.com>
2021-07-07 19:30:51 +0900

    [추가] todoList 지우는 기능

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,27 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event) {
+  //이벤트에 대해, 인자로 명시한 event에서 이벤트에 대한 정보를 얻어올 수 있다.
+  //event.target은 그 이벤트가 발생한 html요소를 가리켜준다.
+  //html요소.parentElement는 그 요소를 감싸는 부모요소를 가리킨다!
+  const li = event.target.parentElement;
+  li.remove(); //해당 html요소를 지워버린다!
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span); //생성한 li태그 안에 span태그를 집어넣는다!
   span.innerText = newTodo; //생성된 span태그 안에 인자로 받아온 toDo집어넣기
+
+  const button = document.createElement("button");
+  button.innerText = "❌"; //생성된 button 태그 안에 글자 집어넣기
+
+  li.appendChild(span); //생성한 li태그 안에 span태그를 집어넣는다!
+  li.appendChild(button); //생성한 li태그 안에 button태그를 집어넣는다!
+
+  button.addEventListener("click", deleteToDo);
+
   toDoList.appendChild(li); //그렇게 만들어놓은 toDo 하나를, toDoList 안에 집어넣기!
 }
 
